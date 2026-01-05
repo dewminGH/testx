086c155d55de2e5aa519e71a1759aa123a5e1554
Tharunda Dewmin <tharunda@creatit.com.au>
2026-01-05 12:02:35 +0530

    xx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
-import Image from "next/image";
+"use client";
 
+import Image from "next/image";
+import { useEffect } from "react";
+import ReactGA from "react-ga4";
 export default function Home() {
+  useEffect(() => {
+    ReactGA.initialize("G-RVHNCRW1G8");
+  }, []);
+  const x = () => {
+    console.log("xxx");
+    ReactGA.event("xx-test-identify", {
+      category: "web-v3-cart",
+      label: `web-v3-nextJS -user id:dewmin`,
+      value: 1,
+    });
+  };
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -12,6 +27,7 @@ export default function Home() {
           height={20}
           priority
         />
+        <button onClick={() => x()}>xsxxxx</button>
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
